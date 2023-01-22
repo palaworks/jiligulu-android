7e94aa838e2837d37cce3f920ce092cc15218dbc
Thaumy <thaumy@outlook.com>
2023-01-22 22:10:42 +0800

    + local comment db & + local post db

---
+++ app/src/main/kotlin/data/db/LocalComment.kt
@@ -0,0 +1,57 @@
+package data.db
+
+import android.content.Context
+import androidx.room.*
+import unilang.alias.i64
+import java.util.Date
+
+@Entity(tableName = "local_comment")
+data class LocalComment(
+    @PrimaryKey val id: i64,
+    @ColumnInfo(name = "body") val body: String,
+    @ColumnInfo(name = "create_time") val createTime: Date,
+)
+
+@Dao
+interface LocalCommentDao {
+    @Query("SELECT * FROM local_comment WHERE id = (:id)")
+    fun maybe(id: i64): LocalComment?
+
+    @Query("SELECT * FROM local_comment")
+    fun getAll(): List<LocalComment>
+
+    @Insert
+    fun insert(localComment: LocalComment)
+
+    @Update
+    fun update(localComment: LocalComment)
+
+    @Delete
+    fun delete(localComment: LocalComment)
+}
+
+@Database(entities = [LocalComment::class], version = 1)
+abstract class LocalCommentDatabase : RoomDatabase() {
+    abstract fun localCommentDao(): LocalCommentDao
+
+    companion object {
+        private var INSTANCE: LocalCommentDatabase? = null
+        fun getDatabase(ctx: Context): LocalCommentDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE =
+                        Room.databaseBuilder(
+                            ctx,
+                            LocalCommentDatabase::class.java,
+                            "local_comment_database"
+                        )
+                            //TODO remove this for better performance
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
+}
---
+++ app/src/main/kotlin/data/db/LocalPost.kt
@@ -0,0 +1,59 @@
+package data.db
+
+import android.content.Context
+import androidx.room.*
+import unilang.alias.i64
+import java.util.Date
+
+@Entity(tableName = "local_post")
+data class LocalPost(
+    @PrimaryKey val id: i64,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "body") val body: String,
+    @ColumnInfo(name = "create_time") val createTime: Date,
+    @ColumnInfo(name = "modify_time") val modifyTime: Date,
+)
+
+@Dao
+interface LocalPostDao {
+    @Query("SELECT * FROM local_post WHERE id = (:id)")
+    fun maybe(id: i64): LocalPost?
+
+    @Query("SELECT * FROM local_post")
+    fun getAll(): List<LocalPost>
+
+    @Insert
+    fun insert(localPost: LocalPost)
+
+    @Update
+    fun update(localPost: LocalPost)
+
+    @Delete
+    fun delete(localPost: LocalPost)
+}
+
+@Database(entities = [LocalPost::class], version = 1)
+abstract class LocalPostDatabase : RoomDatabase() {
+    abstract fun localPostDao(): LocalPostDao
+
+    companion object {
+        private var INSTANCE: LocalPostDatabase? = null
+        fun getDatabase(ctx: Context): LocalPostDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE =
+                        Room.databaseBuilder(
+                            ctx,
+                            LocalPostDatabase::class.java,
+                            "local_post_database"
+                        )
+                            //TODO remove this for better performance
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
+}
