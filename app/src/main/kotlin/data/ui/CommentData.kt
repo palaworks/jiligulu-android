985f204b25b8eb48d5cc834e201819c1c2f6d04b
Thaumy <thaumy@outlook.com>
2023-01-23 18:11:12 +0800

    + exts for PostData and CommentData

--- app/src/main/kotlin/data/ui/CommentData.kt
+++ app/src/main/kotlin/data/ui/CommentData.kt
@@ -1,10 +1,23 @@
 package data.ui
 
+import data.db.LocalPost
 import java.util.*
 import unilang.alias.*
+import unilang.hash.sha256
+import unilang.time.Iso8601
+import unilang.time.toDate
 
 data class CommentData(
     val id: i64,
     val body: String,
     val createTime: Date
 )
+
+fun CommentData(localPost: LocalPost) =
+    CommentData(
+        localPost.id,
+        localPost.body,
+        localPost.createTime
+    )
+
+fun CommentData.sha256() = this.body.sha256()
--- app/src/main/kotlin/data/ui/PostData.kt
+++ app/src/main/kotlin/data/ui/PostData.kt
@@ -1,7 +1,11 @@
 package data.ui
 
+import data.db.LocalPost
 import java.util.*
 import unilang.alias.*
+import unilang.hash.sha256
+import unilang.time.Iso8601
+import unilang.time.toDate
 
 data class PostData(
     val id: i64,
@@ -10,3 +14,14 @@ data class PostData(
     val createTime: Date,
     val modifyTime: Date
 )
+
+fun PostData(localPost: LocalPost) =
+    PostData(
+        localPost.id,
+        localPost.title,
+        localPost.body,
+        localPost.createTime,
+        localPost.modifyTime
+    )
+
+fun PostData.sha256() = (this.title + this.body).sha256()
