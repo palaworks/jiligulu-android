e92a6e022a8539bbbe07651a6fc2c58ae7e6e6de
Thaumy <thaumy@outlook.com>
2023-01-24 17:54:47 +0800

    + data source for post screen and comment screen

--- app/src/main/kotlin/component/screen/CommentScreen.kt
+++ app/src/main/kotlin/component/screen/CommentScreen.kt
@@ -1,33 +1,85 @@
 package component.screen
 
+import android.annotation.SuppressLint
 import java.util.*
 import unilang.alias.i64
-import data.ui.CommentData
 import component.CardList
 import component.CommentCard
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import data.db.LocalCommentDatabase
+import data.grpc.CommentService
+import data.ui.CommentData
+import data.ui.sha256
+import kotlinx.coroutines.launch
 
 val commentDataList = List(8) {
     CommentData(
         12384,
         "The quick brown fox jumps over the lazy dog",
+        114514,
+        true,
         Date()
     )
 }
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun CommentScreen(
     contentPadding: PaddingValues,
     navToCommentDiff: (i64) -> Unit,
     navToCommentEditor: (i64) -> Unit,
+    commentService: CommentService
 ) {
+    val localCommentDao = LocalCommentDatabase.getDatabase(LocalContext.current).localCommentDao()
+
+    val load = suspend {
+        val remoteIdSha256Map = commentService.getAllSha256()
+        val localCommentList = localCommentDao.getAll()
+
+        val resolvedCommentList = mutableListOf<CommentData>()
+
+        val conflictCommentList = localCommentList
+            .fold(mutableListOf<CommentData>()) { acc, localComment ->
+                acc.apply {
+                    val localSha256 = CommentData(localComment).sha256()
+                    val remoteSha256 = remoteIdSha256Map[localComment.id]
+                    if (remoteSha256 == null)
+                        acc.add(CommentData(localComment))//local only comment
+                    else {
+                        remoteIdSha256Map.remove(localComment.id)
+                        if (remoteSha256 == localSha256)
+                            resolvedCommentList.add(CommentData(localComment))//resolved
+                        else
+                            acc.add(CommentData(localComment))//conflict
+                    }
+                }
+            } + remoteIdSha256Map.keys
+            .map {
+                commentService.getOne(it).get()//add remote only comment
+            }
+
+        Pair(conflictCommentList, resolvedCommentList)
+    }
+
+    var fullCommentList by remember { mutableStateOf(listOf<CommentData>()) }
+    var conflictCommentList by remember { mutableStateOf(listOf<CommentData>()) }
+
+    val coroutineScope = rememberCoroutineScope()
+    coroutineScope.launch {
+        val (conflict, resolved) = load()
+
+        conflictCommentList = conflict
+        fullCommentList = conflict + resolved
+    }
+
     Column(
         Modifier
             .fillMaxSize()
@@ -37,7 +89,14 @@ fun CommentScreen(
         CardList(
             itemFetcher = { commentDataList },
             itemRender = {
-                CommentCard(navToCommentDiff, navToCommentEditor, it)
+                CommentCard(
+                    if (conflictCommentList.any { x -> x.id == it.id })
+                        Optional.of(navToCommentDiff)
+                    else
+                        Optional.empty(),
+                    navToCommentEditor,
+                    it
+                )
             }
         )
     }
--- app/src/main/kotlin/component/screen/PostScreen.kt
+++ app/src/main/kotlin/component/screen/PostScreen.kt
@@ -1,5 +1,6 @@
 package component.screen
 
+import android.annotation.SuppressLint
 import java.util.*
 import data.ui.PostData
 import unilang.alias.i64
@@ -7,29 +8,70 @@ import component.CardList
 import component.PostCard
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import data.db.LocalPostDatabase
+import data.grpc.PostService
+import data.ui.sha256
+import kotlinx.coroutines.launch
+import unilang.type.notNullThen
+import unilang.type.nullThen
 
-val postDataList = List(8) {
-    PostData(
-        12384,
-        "Hello world!",
-        "The quick brown fox jumps over the lazy dog",
-        Date(),
-        Date()
-    )
-}
-
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun PostScreen(
     contentPadding: PaddingValues,
     navToPostDiff: (i64) -> Unit,
-    navToPostEditor: (i64) -> Unit
+    navToPostEditor: (i64) -> Unit,
+    postService: PostService,
 ) {
+    val localPostDao = LocalPostDatabase.getDatabase(LocalContext.current).localPostDao()
+
+    val load = suspend {
+        val remoteIdSha256Map = postService.getAllSha256()
+        val localPostList = localPostDao.getAll()
+
+        val resolvedPostList = mutableListOf<PostData>()
+
+        val conflictPostList = localPostList
+            .fold(mutableListOf<PostData>()) { acc, localPost ->
+                acc.apply {
+                    val localSha256 = PostData(localPost).sha256()
+                    val remoteSha256 = remoteIdSha256Map[localPost.id]
+                    if (remoteSha256 == null)
+                        acc.add(PostData(localPost))//local only post
+                    else {
+                        remoteIdSha256Map.remove(localPost.id)
+                        if (remoteSha256 == localSha256)
+                            resolvedPostList.add(PostData(localPost))//resolved
+                        else
+                            acc.add(PostData(localPost))//conflict
+                    }
+                }
+            } + remoteIdSha256Map.keys
+            .map {
+                postService.getOne(it).get()//add remote only post
+            }
+
+        Pair(conflictPostList, resolvedPostList)
+    }
+
+    var fullPostList by remember { mutableStateOf(listOf<PostData>()) }
+    var conflictPostList by remember { mutableStateOf(listOf<PostData>()) }
+
+    val coroutineScope = rememberCoroutineScope()
+    coroutineScope.launch {
+        val (conflict, resolved) = load()
+
+        conflictPostList = conflict
+        fullPostList = conflict + resolved
+    }
+
     Column(
         Modifier
             .fillMaxSize()
@@ -37,9 +79,16 @@ fun PostScreen(
             .padding(horizontal = 10.dp)
     ) {
         CardList(
-            itemFetcher = { postDataList },
+            itemFetcher = { fullPostList },
             itemRender = {
-                PostCard(navToPostDiff, navToPostEditor, it)
+                PostCard(
+                    if (conflictPostList.any { x -> x.id == it.id })
+                        Optional.of(navToPostDiff)
+                    else
+                        Optional.empty(),
+                    navToPostEditor,
+                    it,
+                )
             }
         )
     }
