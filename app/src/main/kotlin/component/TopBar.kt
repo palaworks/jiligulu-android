c571e9753bde5be38128b3c5ae63a8ec70abf256
Thaumy <thaumy@outlook.com>
2023-02-01 01:02:48 +0800

    + icons

--- app/src/main/kotlin/component/TopBar.kt
+++ app/src/main/kotlin/component/TopBar.kt
@@ -1,8 +1,14 @@
 package component
 
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.jiligulu.R.drawable.jiligulu
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -16,7 +22,20 @@ fun TopBar(
         )
 
     TopAppBar(
-        title = { Text(title) },
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.size(36.dp),
+                    painter = painterResource(jiligulu),
+                    tint = Color.Unspecified,
+                    contentDescription = "App icon"
+                )
+                Spacer(Modifier.width(10.dp))
+                Text(title)
+            }
+        },
         colors = colors,
         actions = actions
     )
